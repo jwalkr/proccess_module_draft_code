ae28e4f653871ffbb81e724f3a2f3c8fd4ee2cd1
academy_intern <academy_intern@academy-interns-MacBook-Pro.local>
2018-10-05 10:40:02 +0200

    translator

--- src/routes/translator.ts
+++ src/routes/translator.ts
@@ -1,2 +1,18 @@
 
-import {Request, }
+import {Request, Response} from "express";
+//load to my local database file
+import pokemons = require('../db.json' );
+
+export class pokemons{
+
+    //received the express instance from app.ts file
+    public routes(app); void{
+        app.route('/pokemons')
+        .get((req: Request, res: Response)=> {
+            res.status(200).send(pokemons);
+        })
+
+    }
+}
+
+
